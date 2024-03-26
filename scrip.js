b7044750695012a1b55be97b31a1b0216854266d
k4rl1t418 <162450291+k4rl1t418@users.noreply.github.com>
2024-03-26 11:06:32 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,13 @@
+let nombre = "benjamin"
+
+let apellido = "orellana"
+
+let edad = 17
+
+let verdadero = true
+
+let arreglo = ["benjamin","orellana",17];
+
+let objeto = {nombre: "benjamin", apellido : "orellana", edad : 17, verdadero : true};
+console.log("hola mundo")
+console.log(objeto)
